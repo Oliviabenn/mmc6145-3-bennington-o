6e342547153d959f3de91b298857c9d9e543618d
Olivia <oliviabennington@gmail.com>
2023-02-23 10:17:57 -0500

    tests passing

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -52,7 +52,7 @@ export default function Search() {
             name="book-search"
             id="book-search"
             value={query}
-            onChange={ e => setQuery(e.target.value)}
+            onChange={(e) => setQuery(e.target.value)}
             />
           <button type="submit">Submit</button>
         </div>
